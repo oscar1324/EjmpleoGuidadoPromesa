d3275721b58a5b827f57baf2718bca0ac05d33d2
oscar <oscarGradoClase@gmail.com>
2020-12-15 12:45:11 +0100

    Ejercicio 7 y 8 completado

---
+++ js/Ejercicio7.js
@@ -0,0 +1,48 @@
+window.addEventListener("load",() => {
+    document.getElementById("form").addEventListener("submit",manejarEventoSubmit, false )
+}, false);
+
+function manejarEventoSubmit(event){
+    event.preventDefault(); 
+
+    fetch("https://jsonplaceholder.typicode.com/ustrs/") //devuelve una promesa
+ .then(response => {
+     if(response.ok){
+        return response.json();
+     } else {
+         return  "error";
+     }
+ }) //El método json() devuelve otra promesa
+ .then(datosUsuario => mostrarDatosUsuario(datosUsuario)) //then() de la segunda promesa
+ .catch(error => console.error(error));
+
+}
+
+function mostrarDatosUsuario(datosUsuario){
+
+    if(datosUsuario == 'error'){
+        alert ("errorrrr");
+    }else{
+        let filadeLaTabla = document.getElementById("tabla");
+        for(i=0; i < datosUsuario.length;i++){
+           console.log(datosUsuario[i]);
+       
+           let tr = document.createElement("tr");
+           //recordar siempre entre comillas aun que los reconozcan además de los + entre elemento y elemento
+           tr.innerHTML ="<td>"+ datosUsuario[i].id + "</td>" +"<td>" + datosUsuario[i].name +"</td>"  + "<td>" + datosUsuario[i].username +"</td>"  + "<td>" +datosUsuario[i].email +"<td>";
+           filadeLaTabla.appendChild(tr);
+        }
+    }
+     
+
+}
+
+function crearTabla (datos){
+    for(i = 0; i< datos.length; i++);
+    
+    let tabla = document.getElementById("tr");
+    let a1 = document.getElementById("td");
+    let a2 = document.getElementById("td");
+    console.log(datos[i]);
+    
+}
---
+++ js/Ejercicio9.js
@@ -0,0 +1,84 @@
+window.addEventListener("load",() => {
+    document.getElementById("Ver").addEventListener('click',manejarVerAnimales,false);
+    document.getElementById("nuevo").addEventListener('submit',manejarCrearAnimales,false);
+});
+
+/**Promesa: ver animales */
+function manejarVerAnimales(){
+    
+    fetch(url) // ojo
+    .then(response => {
+        if(response.ok){
+            console.log("Entra en if");
+            return response.json();
+        } else {
+            return "error";
+        }
+    })
+    .then(datosRespuesta => DatosAnimales(datosRespuesta))
+    .catch(err => console.error(err));
+}
+
+function DatosAnimales(datosRespuesta){
+    if(datosRespuesta == "error"){
+        alert("Se ha producido un error");
+    } else {
+        let obtenerdiv = document.getElementById("texto");
+        console.log("Entra");
+
+        for(i=0 ; i <datosRespuesta.length; i++){
+            console.log("Entra bucle for");
+            console.log(datosRespuesta[i]);
+
+            let par = document.createElement("p");
+            par.innerHTML = "<p>" + datosRespuesta[i].Tipo + "</p>"  + "<p>" + datosRespuesta[i].Observacion + "</p>" + "<p>" + datosRespuesta[i].Rasgos + "</p>" +  " ___________";
+            // revisar mayusculas
+            obtenerdiv.appendChild(par);
+        };
+    }
+}
+
+/**Promesa: Crear nuevo Animal */
+
+
+function manejarCrearAnimales(){
+    let url = "http://localhost:3000/animales";
+    let nuevoAnimal={
+        id:document.getElementById("idAnimal").value ,
+        Tipo:document.getElementById("tipoAnimal").value ,
+        Nombre:document.getElementById("nombre").value ,
+        Observacion:document.getElementById("observacion").value ,
+        Rasgos:document.getElementById("rasgos").value 
+    };
+    let init = {
+        method:'POST',
+        body:JSON.stringify(nuevoAnimal),
+        headers:{'Content-Type': 'application/json'}
+    };
+console.log("entra 1");
+    fetch(url,init)
+    .then(response =>{
+        if(response.ok){
+            return response.json()
+        }else{
+            return "Error"
+        }
+    })
+    .then(datosNuevos => creacionAnimal(datosNuevos))
+    .catch(err => console.error(err))
+}
+
+function creacionAnimal(datosNuevos){
+
+    let parrafo = document.getElementById("texto");
+
+    if(datosNuevos == "Error"){
+        return "Se ha producido un error, vuelva a intentarlo"
+    } else {
+        console.log(datosNuevos);
+
+    }
+
+}
+
+
